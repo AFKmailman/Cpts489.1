520ad18b8401b7fd181dbd327667a0a42e5ed5e5
clambe <camden.lambe@wsu.edu>
2020-02-20 05:24:07 -0800

    Updates

---
+++ precache-manifest.05068d26f39eaf036bcbe7eaf20a5886.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03534f694ed073e17fa088d3325013b0",
+    "url": "/Cpts489.1/index.html"
+  },
+  {
+    "revision": "dcb533119432fcfd1e94",
+    "url": "/Cpts489.1/static/css/2.0cd588bb.chunk.css"
+  },
+  {
+    "revision": "823e3c62006b89f61258",
+    "url": "/Cpts489.1/static/css/main.e33910ca.chunk.css"
+  },
+  {
+    "revision": "dcb533119432fcfd1e94",
+    "url": "/Cpts489.1/static/js/2.da7c8579.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Cpts489.1/static/js/2.da7c8579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823e3c62006b89f61258",
+    "url": "/Cpts489.1/static/js/main.183c22f6.chunk.js"
+  },
+  {
+    "revision": "d4a59026f7714c1cc2d6",
+    "url": "/Cpts489.1/static/js/runtime-main.a702ff9d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Cpts489.1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Cpts489.1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Cpts489.1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Cpts489.1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Cpts489.1/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
